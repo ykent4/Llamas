3286daa15309dfaf61ecdabd39d0c5cb302cf971
Kent Wang <ykentwang4@gmail.com>
2018-06-22 15:57:41 -0700

    additional problems completed

---
+++ tree/removeInvalidParens.js
@@ -0,0 +1,46 @@
+//Remove the minimum number of invalid parentheses in order to make the input string valid. Return all possible results.
+
+function removeInvalidParens (string) {
+
+  var result = [];
+  var maxLength = 0;
+
+  //recurse through the function
+  function innerHelper(inputString, currentString, leftCount) {
+
+    //base case
+    if (inputString.length === 0) {
+      if(currentString.length > maxLength) {
+        maxLength = currentString.length;
+      }
+
+      if(currentString.length === maxLength && !result.includes(currentString)) {
+        result.push(currentString);
+      }
+      return;
+    }
+
+    var char = inputString[0];
+
+    if(char === '(') {
+      innerHelper(inputString.substring(1), currentString+'(', leftCount+1);
+
+    } else if(char === ')') {
+      if(leftCount > 0) {
+        innerHelper(inputString.substring(1), currentString+')', leftCount-1); 
+      }
+      innerHelper(inputString.substring(1), currentString, leftCount);
+
+    } else {
+      innerHelper(inputString.substring(1), currentString, leftCount);
+    }
+    
+  }
+
+  innerHelper(string, '', 0);
+
+  return result;
+
+}
+
+
